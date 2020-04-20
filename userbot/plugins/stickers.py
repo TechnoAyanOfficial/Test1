e6aebb1c9cfc53959a410c550290815cab5d4134
AaryanBots <63472876+AaryanBots@users.noreply.github.com>
2020-04-21 02:11:43 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -29,7 +29,7 @@ from telethon.tl.types import (
 from userbot.utils import admin_cmd
 from userbot import ALIVE_NAME
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "GujjuBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "AaryanBot"
 FILLED_UP_DADDY = "Invalid pack selected."
 
 @borg.on(admin_cmd(pattern="kang ?(.*)"))
@@ -50,28 +50,28 @@ async def _(event):
         user.first_name = user.id
     pack = 1
     userid = event.from_id
-    #packname = f"JayukeStickers"
-    #packshortname = f"GujjuBot_{userid}_ns"  # format: Uni_Borg_userid
-    if userid == 948408212:
-        packname = f"JayukeStickers"
-        packshortname = "Jayu_ke_locker_me"
+    #packname = f"TheRajput"
+    #packshortname = f"AaryanBot_{userid}_ns"  # format: Uni_Borg_userid
+    if userid == 1025131428:
+        packname = f"AaryanStickers"
+        packshortname = "Aaryan_Property"
     else:
-        packname = f"GujjuBot{userid}"
-        packshortname = f"GujjuBot_{userid}_ns"
-    await event.edit("`Look dat way,it's a gurl!\nMeanwhile, lemme kang this stcker over hehe ヽ༼ ಠ益ಠ ༽ﾉ`")
+        packname = f"AaryanBots{userid}"
+        packshortname = f"AaryanBots_{userid}_ns"
+    await event.edit("`Look dat way,it's a gurl!\nMeanwhile, lemme Buy this sticker over here ヽ༼ ಠ益ಠ ༽ﾉ`")
 
     is_a_s = is_it_animated_sticker(reply_message)
-    file_ext_ns_ion = "GujjuBot_Sticker.png"
+    file_ext_ns_ion = "AaryanBot_Sticker.png"
     file = await borg.download_file(reply_message.media)
     uploaded_sticker = None
     if is_a_s:
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
         packname = f"{userid}'s @AnimatedStickersGroup"
-        if userid == 948408212:
-            packshortname = "Jayu_Animated"
+        if userid == 1025131428:
+            packshortname = "Aaryan_Animated"
         else:
-            packshortname = f"GujjuBot_{userid}_an" # format: Uni_Borg_userid
+            packshortname = f"AaryanBot_{userid}_an" # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
@@ -131,14 +131,14 @@ async def _(event):
                 while response.text == FILLED_UP_DADDY:
                     pack += 1
                     prevv = int(pack) - 1
-                    packname = f"{user.first_name}'s GujjuBot Vol.{pack}"
+                    packname = f"{user.first_name}'s AaryanBot Vol.{pack}"
                     packshortname = f"Vol_{pack}_with_{user.first_name}"
-                    #if userid == 948408212:
-                       # packname = f"{user.first_name}'s GujjuBot Vol.{pack}"
-                       # packshortname = "Vol._{pack}_Jayu_ke_locker_me"
+                    #if userid == 1025131428:
+                       # packname = f"{user.first_name}'s AaryanBot Vol.{pack}"
+                       # packshortname = "Vol._{pack}_Aaryan_Sticker"
                    # else:
-                       # packname = f"Vol._{pack}_GujjuBot{userid}"
-                        #packshortname = f"Vol._{pack}_GujjuBot_{userid}_ns"
+                       # packname = f"Vol._{pack}_AaryanBot{userid}"
+                        #packshortname = f"Vol._{pack}_AaryanBot_{userid}_ns"
                     if not await stickerset_exists(bot_conv, packshortname):
                         await event.edit("**Pack No. **" + str(prevv) + "** full! Making a new Pack, Vol. **" + str(pack))
                         if is_a_s:
@@ -249,7 +249,7 @@ async def _(event):
             return
         is_a_s = is_it_animated_sticker(reply_message)
         file_ext_ns_ion = "webp"
-        file_caption = "https://t.me/RoseSupport/33801"
+        file_caption = "https://t.me/Therajput"
         if is_a_s:
             file_ext_ns_ion = "tgs"
             file_caption = "Forward the ZIP file to @AnimatedStickersRoBot to get lottIE JSON containing the vector information."
