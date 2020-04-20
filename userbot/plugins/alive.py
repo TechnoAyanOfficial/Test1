73eebe6bb244b35fb6f3ccc37122f33445832142
AaryanBots <63472876+AaryanBots@users.noreply.github.com>
2020-04-20 15:42:27 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -15,7 +15,7 @@ async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("`░█─░█ █▀▀ █── █── █▀▀█ 
 ░█▀▀█ █▀▀ █── █── █──█ 
-░█─░█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀  Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
+░█─░█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ \n\n Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
                      f"`My peru owner`: {DEFAULTUSER}\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @TheRajput\n"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
